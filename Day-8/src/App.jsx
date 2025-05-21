b9653747b4b82e3191e3832eaf82a665b3a3ccc8
Nikin M <nikinmaheshkumar@gmail.com>
2025-05-21 13:39:31 +0530

    Add MovieCard Component

--- Day-8/src/App.jsx
+++ Day-8/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import MovieCard from './components/MovieCard';
 
 function App() {
 
+  const movieNumber = 2;
   return (
     <>
-      <h1>Hello</h1>
+      <div className='flex justify-center justify-items-center content-center'>
+
+        {movieNumber === 1 ? (<MovieCard movie={{ title: "Endgame", release_date: "2024" }} />) : (
+        <MovieCard movie={{ title: "Infinty War", release_date: "2024" }} />)}
+        
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Day-8/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home()
+{
+    const movies = [
+        {id: 1, title: "John wick", release_date: "2023"},
+        {id: 2, title: "Terminator", release_date: "1990"},
+        {id: 3, title: "Endgame", release_date: "2019"},
+        {id: 4, title: "Sinners", release_date: "2024"},
+    ]
+
+    return (
+        <div className="home"></div>
+    );
+}
