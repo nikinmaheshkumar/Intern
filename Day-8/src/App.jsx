ee2680acb0a8204c10eda8c0b5835983770f4567
Nikin M <nikinmaheshkumar@gmail.com>
2025-05-21 13:50:59 +0530

    Add Home Page

--- Day-8/src/App.jsx
+++ Day-8/src/App.jsx
@@ -1,17 +1,10 @@
 import './App.css'
-import MovieCard from './components/MovieCard';
+import Home from './pages/Home'
 
 function App() {
-
-  const movieNumber = 2;
   return (
     <>
-      <div className='flex justify-center justify-items-center content-center'>
-
-        {movieNumber === 1 ? (<MovieCard movie={{ title: "Endgame", release_date: "2024" }} />) : (
-        <MovieCard movie={{ title: "Infinty War", release_date: "2024" }} />)}
-        
-      </div>
+      <Home />
     </>
   );
 }
--- Day-8/src/pages/Home.jsx
+++ Day-8/src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import MovieCard from '../components/MovieCard'
+
 function Home()
 {
     const movies = [
@@ -6,8 +8,25 @@ function Home()
         {id: 3, title: "Endgame", release_date: "2019"},
         {id: 4, title: "Sinners", release_date: "2024"},
     ]
+    const handleSearch = () => {
+
+    }
 
     return (
-        <div className="home"></div>
+        <div className="home flex flex-col align-middle items-center mt-12">
+            <form onSubmit={handleSearch} className='search-input'>
+                <input type='text' placeholder='Search for movies ....' className='search-input'/>
+                <button type='submit' className='search-btn'>Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
+            ))}
+            </div>
+        </div>
     );
 }
+
+
+export default Home
