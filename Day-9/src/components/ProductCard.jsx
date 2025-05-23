12039f493501ab6b67ed3b885ae97ff7a10c29a9
Nikin M <nikinmaheshkumar@gmail.com>
2025-05-23 10:48:34 +0530

    Day-10 Init

--- Day-9/src/components/ProductCard.jsx
+++ Day-9/src/components/ProductCard.jsx
@@ -5,23 +5,25 @@ function ProductCard({ product }) {
 
     const navigate = useNavigate();
   return (
-    <div className="bg-[#32004f] text-white rounded-xl p-4 w-full max-w-xs flex flex-col transition duration-300 hover:scale-105 hover:shadow-lg">
-      <div className="flex flex-col items-center px-2">
-        <h2 className="text-lg text-white text-center font-bold">{product.title}</h2>
-      </div>
-      <div className="mt-4 overflow-hidden rounded-xl">
+   <div className="bg-[#32004f] text-white rounded-xl p-4 w-full max-w-xs flex flex-col transition duration-300 hover:scale-105 hover:shadow-lg mb-10">
+      <div className="mt-2 overflow-hidden rounded-xl bg-white p-2">
         <img
           src={product.image}
           alt={product.title}
-          className="w-full object-contain h-[200px] rounded-2xl bg-white"
+          className="w-full object-contain h-[200px] rounded-xl"
         />
       </div>
-      <p className="text-md font-semibold mt-3 text-black text-center">${product.price}</p>
-      <div className="mt-auto pt-4 flex justify-center space-x-3">
-        <button className="px-4 py-2 bg-[#190028] text-white rounded-lg text-sm font-medium " onClick={() => navigate(`/product/${product.id}`)}>
-          ℹ️  More Details
+      <div className="mt-4 text-center">
+        <h2 className="text-lg font-bold">{product.title}</h2>
+      </div>
+      <p className="text-md font-semibold mt-2 text-white text-center">${product.price}</p>
+      <div className="mt-auto pt-4 flex flex-col sm:flex-row sm:justify-center sm:space-x-3 space-y-2 sm:space-y-0">
+        <button
+          className="px-4 py-2 bg-[#190028] text-white rounded-lg text-sm font-medium"
+          onClick={() => navigate(`/product/${product.id}`)}
+        >
+          ℹ️ More Details
         </button>
-       
       </div>
     </div>
   );
