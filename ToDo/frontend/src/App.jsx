a3ffa5aac338574d6e44e1f15f31e10dc4d1bffe
Nikin M <nikinmaheshkumar@gmail.com>
2025-06-10 17:30:31 +0530

    feat Navigation

--- ToDo/frontend/src/App.jsx
+++ ToDo/frontend/src/App.jsx
@@ -1,11 +1,32 @@
 import './index.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
-  
+
   return (
-    <>
-      <h1 className='text-center text-blue-300 text-4xl'>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<RegisterAndLogout />} />
+        <Route path='*' element={<NotFound />} />
+       </Routes>
+    </BrowserRouter>
   )
 }
 
