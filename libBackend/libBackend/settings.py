c094f5adcbebd338feb90cd6bee378477ee779cf
Nikin M <nikinmaheshkumar@gmail.com>
2025-06-24 13:50:51 +0530

    old changes

--- libBackend/libBackend/settings.py
+++ libBackend/libBackend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'library',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,15 @@ WSGI_APPLICATION = 'libBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library_db',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
